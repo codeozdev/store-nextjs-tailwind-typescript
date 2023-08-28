efee2d398f706603b664a0e1bda62fe9bdd34428
codeoz <codeoz@proton.me>
2023-08-28 05:15:35 +0300

    readme

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -41,7 +41,7 @@ export const AuthContextProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser)
-      console.log(currentUser)
+      // console.log(currentUser)
     })
     return () => unsubscribe()
   }, [user])
